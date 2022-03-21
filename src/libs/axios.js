0364199ddc14f67a3d64d4360573274aa8b47189
Jose Guerra <jose1894@gmail.com>
2022-03-21 05:29:34 -0500

    first commit

---
+++ src/libs/axios.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+// axios
+import axios from 'axios'
+
+const axiosIns = axios.create({
+  // You can add your headers here
+  // ================================
+  // baseURL: 'https://some-domain.com/api/',
+  // timeout: 1000,
+  // headers: {'X-Custom-Header': 'foobar'}
+  baseURL: 'http://localhost:8080/'
+})
+
+Vue.prototype.$http = axiosIns
+
+export default axiosIns
